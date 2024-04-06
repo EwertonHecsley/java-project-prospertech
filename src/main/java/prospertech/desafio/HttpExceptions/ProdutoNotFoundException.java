159eeee1734b3d6bbb4257c636d12c67f3d22c8e
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-04-06 18:21:36 -0300

    endpoint detalhar um produto

---
+++ src/main/java/prospertech/desafio/HttpExceptions/ProdutoNotFoundException.java
@@ -0,0 +1,11 @@
+package prospertech.desafio.HttpExceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProdutoNotFoundException extends RuntimeException {
+    public ProdutoNotFoundException(Integer id) {
+        super("Produto não encontrado com o Id: " + id);
+    }
+}
--- src/main/java/prospertech/desafio/controller/ProdutoController.java
+++ src/main/java/prospertech/desafio/controller/ProdutoController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import prospertech.desafio.HttpExceptions.ProdutoNotFoundException;
 import prospertech.desafio.model.Produto;
 import prospertech.desafio.repository.ProdutoRepository;
 
@@ -16,6 +18,12 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @GetMapping("/{id}")
+    public Produto detalhar(@PathVariable Integer id){
+       return produtoRepository.findById(id)
+       .orElseThrow(()-> new ProdutoNotFoundException(id));
+    }
 
     @GetMapping
     public List<Produto> listar(){
